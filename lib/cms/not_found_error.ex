2af5e8485c06a68fe28ae749bdba893184bdf0ba
Brian Alexander <balexand@gmail.com>
2022-03-26 17:35:30 -0600

    Update not_found_error.ex

--- lib/cms/not_found_error.ex
+++ lib/cms/not_found_error.ex
@@ -8,9 +8,13 @@ defmodule CMS.NotFoundError do
   defexception [:message]
 end
 
-if function_exported?(Plug, :__info__, 1) do
-  defimpl Plug.Exception, for: CMS.NotFoundError do
-    def status(_), do: :not_found
-    def actions(_), do: []
-  end
+case Code.ensure_compiled(Plug.Exception) do
+  {:module, _} ->
+    defimpl Plug.Exception, for: CMS.NotFoundError do
+      def status(_), do: :not_found
+      def actions(_), do: []
+    end
+
+  {:error, _} ->
+    nil
 end
